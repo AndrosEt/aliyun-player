ef97f50c1d06c12b0d1236c660f3db37654415e5
AndrosEt <1756238821@qq.com>
2020-11-24 23:40:27 +0800

    update path

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,8 @@
 //       publicPath: '/'
 //     }
 module.exports = {
-  outputDir: 'docs'
+  outputDir: 'docs',
+  publicPath: process.env.NODE_ENV === "production" ? "/aliyun-player" : "/"
   // options...
   // publicPath: 'https://whiter.cc/cached-assets'
   // publicPath: process.env.NODE_ENV === 'production'
